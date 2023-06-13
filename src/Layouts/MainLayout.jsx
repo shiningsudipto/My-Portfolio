e3a8398ca1cf613fc62842ad3a4ee6575bf685d4
Sudipto Das <shiningsudipto@gmail.com>
2023-06-14 01:36:51 +0600

    daisy ui installed

--- src/Layouts/MainLayout.jsx
+++ src/Layouts/MainLayout.jsx
@@ -1,10 +1,14 @@
 import { Outlet } from "react-router-dom";
+import Navbar from "../pages/shared/Navbar";
+import Footer from "../pages/shared/Footer";
 
 const MainLayout = () => {
     return (
         <div>
+            <Navbar />
             <h1>Main Layout</h1>
             <Outlet />
+            <Footer />
         </div>
     );
 };
